8260a6c3fd106a55764696248ad9d3d482ec3007
Caolan Gilroy <caolangilroy@gmail.com>
2023-12-22 00:17:48 +0000

    OnClick added to tiles for all games

--- src/NumberTile.tsx
+++ src/NumberTile.tsx
@@ -1,3 +1,4 @@
+import { on } from 'events';
 import React from 'react'
 
 interface NumberTileProps {
@@ -15,18 +16,27 @@ export default function NumberTile({num}: NumberTileProps) {
     }
     const style = { backgroundColor: colour, fontSize: "3rem" }
     const multipleStyle = { backgroundColor: multipleColour, fontSize: "1.5rem" }
+    function onClick() {
+        console.log(num)
+    }
 
+    function onClickX2() {
+        console.log(num*2)
+    }
+    function onClickX3() {
+        console.log(num*3)
+    }
   return (
     <div className='number-tile'>
         <div className='number-tile-container'>
-            <div className='top-number-tile-element' style={style}>
+            <div className='top-number-tile-element' style={style} onClick={onClick}>
                 {num}
             </div>
             <div className='bottom-number-tile-elements'>
-                <div className='number-tile-multiple' style={multipleStyle}>
+                <div className='number-tile-multiple' style={multipleStyle} onClick={onClickX3}>
                     X3
                 </div>
-                <div className='number-tile-multiple' style={multipleStyle}>
+                <div className='number-tile-multiple' style={multipleStyle} onClick={onClickX2}>
                     X2
                 </div>
             </div>
