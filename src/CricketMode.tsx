2ebc3eb8cbe298121e6053cc92f169b6b37135c0
Caolan Gilroy <caolangilroy@gmail.com>
2023-12-22 23:04:49 +0000

    Basic Cricket Mode Functionality complete for one player

--- src/CricketMode.tsx
+++ src/CricketMode.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ScoreBoard from './CricketScoreBoard'
 import NumberTile from './NumberTile'
 import Bull from './Bull';
@@ -11,17 +11,23 @@ interface ScoreObject {
 
 
 export default function CricketMode() {
-    const [scoreArray, setScore ] = useState<ScoreObject[]>(Array.from({ length: 6 }, (_, index) =>  ( {value: (index + 15), score: 3})))
+    const initialScores = Array.from({ length: 6 }, (_, index) =>  ( {value: (index + 15), score: 3}));
+    
+    const [scoreArray, setScore ] = useState<ScoreObject[]>([...initialScores, {value:50, score:3}] );
     const myArray1: number[] = Array.from({ length: 6 }, (_, index) => index + 15);
     const numberTiles: JSX.Element[] = [];
     // Bull needed to be added to scoreArray
+    useEffect(() => {
+      const initialScoreArray = Array.from({ length: 6 }, (_, index) =>  ( {value: (index + 15), score: 3}))
+      setScore([...scoreArray, {value: 50, score: 3}])
+    }, [])
     // const addBullToScoreArray = [...scoreArray, {value: 50, score: 3}]
-    // setScore(addBullToScoreArray)
+    // setScore([...scoreArray, {value: 50, score: 3}])
     myArray1.reverse();
     function onTileClick(num: number) {
         let scoreArrayCopy: ScoreObject[] = [... scoreArray]
         let matchingObject = scoreArrayCopy.find((obj)=> obj.value === num)
-        if (matchingObject) {
+        if (matchingObject && matchingObject.score > 0) {
           console.log('before:')
 
           console.log(matchingObject.score)
@@ -37,7 +43,7 @@ export default function CricketMode() {
     function onX3Click(num: number) {
       let scoreArrayCopy: ScoreObject[] = [... scoreArray]
       let matchingObject = scoreArrayCopy.find((obj)=> obj.value === num)
-      if (matchingObject) matchingObject.score = matchingObject.score - 3
+      if (matchingObject && matchingObject.score > 0) matchingObject.score = matchingObject.score - 3
       setScore(scoreArrayCopy)
       console.log(scoreArrayCopy)
 
@@ -46,7 +52,7 @@ export default function CricketMode() {
     function onX2Click(num: number) {
       let scoreArrayCopy: ScoreObject[] = [... scoreArray]
       let matchingObject = scoreArrayCopy.find((obj)=> obj.value === num)
-      if (matchingObject) matchingObject.score = matchingObject.score - 2
+      if (matchingObject && matchingObject.score > 0) matchingObject.score = matchingObject.score - 2
       setScore(scoreArrayCopy)
       console.log(scoreArrayCopy)
     }
@@ -62,7 +68,7 @@ export default function CricketMode() {
   return (
     <div className='standard-board'>
         <div className='standard-board-first-row'><CricketScoreBoard key='Player 1' name="Caolan" scoreArray={scoreArray}/> <CricketScoreBoard key='Player 2' name='Dad' scoreArray={scoreArray}/></div>
-      <div className='main'><div className='game-container'><Bull isCricketMode={true} bull={50} onX2Click={onX2Click} onTileClick={onTileClick}/><Bull isCricketMode={true} bull={25}/>{numberTiles}</div></div>
+      <div className='main'><div className='game-container'><Bull isCricketMode={true} bull={50} onX2Click={onX2Click}/><Bull isCricketMode={true} bull={25} onTileClick={onTileClick}/>{numberTiles}</div></div>
     </div>
   )
 }
--- src/CricketScoreBoard.tsx
+++ src/CricketScoreBoard.tsx
@@ -3,44 +3,42 @@ import CricketCount from './CricketCount';
 
 interface CricketScoreBoardProps {
     name: string;
-    // isCricketMode?: boolean;
-    // isAroundTheBoardMode?: boolean;
     scoreArray: {value:number, score:number}[];
-    // testScoreArray: object[];
 }
 
     
 export default function CricketScoreBoard({scoreArray, name}: CricketScoreBoardProps) {
-    let scoreTile = <div>  </div>
+    // let scoreTile = <div>  </div>
     let cricketStyle = {width: '40vw'}
-        const myArray1: number[] = Array.from({ length: 6 }, (_, index) => index + 15);
-        myArray1.reverse();
-        const [cricketScores, setCricketScores] = useState<JSX.Element[]>([])
-        useEffect(()=> {
-            let newCricketScores: JSX.Element[] = [] 
-            let bullMatch = scoreArray.find((obj)=> obj.value === 50)
-            console.log('@@@@@SCORE ARRAY')
-            console.log(scoreArray)
+    const myArray1: number[] = Array.from({ length: 6 }, (_, index) => index + 15);
+    myArray1.reverse();
+    const [cricketScores, setCricketScores] = useState<JSX.Element[]>([])
+    useEffect(()=> {
+        let newCricketScores: JSX.Element[] = [] 
+        let bullMatch = scoreArray.find((obj)=> obj.value === 50)
+        console.log(bullMatch)
+        console.log('@@@@@SCORE ARRAY')
+        console.log(scoreArray)
 
-            newCricketScores.push(<CricketCount key='bullScore' num={50} testScore={bullMatch? bullMatch.score: 3}/>)
-            myArray1.forEach((value, index) => {
-                let objectMatch = scoreArray.find((obj)=> obj.value === value)
-                let keyname = 'scoreFor' + value
-                newCricketScores.push(<CricketCount key={keyname} num={value} testScore={objectMatch? objectMatch.score:3}/>)
-                setCricketScores(newCricketScores)
-            })
+        newCricketScores.push(<CricketCount key='bullScore' num={50} testScore={bullMatch? bullMatch.score: 3}/>)
+        myArray1.forEach((value, index) => {
+            let objectMatch = scoreArray.find((obj)=> obj.value === value)
+            let keyname = 'scoreFor' + value
+            newCricketScores.push(<CricketCount key={keyname} num={value} testScore={objectMatch? objectMatch.score:3}/>)
+        })
+        setCricketScores(newCricketScores)
+
+        
+    }, [scoreArray])
             
-        }, [scoreArray])
-        scoreTile = <div className='score-tile' style={cricketStyle}>
-                        <div className='top-score-tile-element'>{name}</div>
-                            <div className='cricket-score-container'>
-                                {cricketScores}
-                            </div>
-                        </div>
-                
     return (
     <div className='score-tile-container'>
-        {scoreTile}
+        <div className='score-tile' style={cricketStyle}>
+                    <div className='top-score-tile-element'>{name}</div>
+                        <div className='cricket-score-container'>
+                            {cricketScores}
+                        </div>
+                    </div>
     </div>
   )
 }
