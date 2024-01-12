2561e0fe206136b62167d46543074141252c35cb
Caolan Gilroy <caolangilroy@gmail.com>
2024-01-12 16:29:15 +0000

    ATB Win/Lose Functionality Added

--- src/AroundTheBoardMode.tsx
+++ src/AroundTheBoardMode.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import NumberTile from './NumberTile';
+import Bull from './Bull';
 import ScoreBoard from './ScoreBoard';
 import AroundTheBoardVisibleTiles from './AroundTheBoardVisibleTiles';
 
@@ -9,10 +10,13 @@ export default function AroundTheBoardMode() {
     const [player2Position, setPlayer2Postiion]= useState(1);
     const [shotCounter, setShotCounter] = useState<number>(0);
     const [player1IsNext, setPlayer1IsNext] = useState<boolean>(true);
+    const [player1Wins, setPlayer1Wins] = useState<boolean>(false);
+    const [player2Wins, setPlayer2Wins] = useState<boolean>(false);
 
     
 
     function onTileClick(index: number) {
+        if (player1Wins || player2Wins) return;
         console.log('clicked')
         let newShotCounter = shotCounter + 1;
         console.log(newShotCounter)
@@ -20,6 +24,7 @@ export default function AroundTheBoardMode() {
         const playerPosition = player1IsNext? player1Position : player2Position
         if (index === playerPosition) {
             let nextPosition = playerPosition + 1
+            if (nextPosition > 20) player1IsNext? setPlayer1Wins(true) : setPlayer2Wins(true)
             player1IsNext? setPlayer1Postiion(nextPosition) : setPlayer2Postiion(nextPosition)
         }
 
@@ -30,9 +35,17 @@ export default function AroundTheBoardMode() {
         }
 
     }
-    function onClick() {
+    function onMiss() {
         // setTestCount(testCount + 1)
         console.log('clickd')
+        let newShotCounter = shotCounter + 1;
+        console.log(newShotCounter)
+        setShotCounter(newShotCounter);
+        if (newShotCounter >= 3) {
+            // console.log('shotCounter >= 2 + shotCounter: ' + shotCounter)
+            setShotCounter(0);
+            setPlayer1IsNext(!player1IsNext);
+        }
     }
   
     function onX3Click() {
@@ -44,7 +57,7 @@ export default function AroundTheBoardMode() {
       }
   return (
     <div className='standard-board'>
-        <div className='standard-board-first-row'><ScoreBoard name="Caolan" isAroundTheBoardMode={true} playerTurn={player1IsNext}/> <ScoreBoard name='Dad' isAroundTheBoardMode={true} playerTurn={!player1IsNext}/></div>
+        <div className='standard-board-first-row'><ScoreBoard name="Caolan" isAroundTheBoardMode={true} playerTurn={player1IsNext} winner={player1Wins}/> <ScoreBoard name='Dad' isAroundTheBoardMode={true} playerTurn={!player1IsNext} winner={player2Wins}/></div>
 
         <div className='main'>
             <div className='game-container'>
@@ -52,7 +65,10 @@ export default function AroundTheBoardMode() {
                 {!player1IsNext &&<AroundTheBoardVisibleTiles current={player2Position} onTileClick={onTileClick} onX2Click={onX2Click} onX3Click={onX3Click}/>}
 
             </div>
+
         </div>
+        <div className='standard-board-first-row'><div className='miss' onClick={onMiss}> Miss </div></div>
+
     </div>
   )
 }
