d6783fcdabf8626b55a38d218badd6350d398a74
Caolan Gilroy <caolangilroy@gmail.com>
2024-01-12 00:37:36 +0000

    Around The Board Mode Fixed for Player 1, Player 2 needs Implementing

--- src/AroundTheBoardMode.tsx
+++ src/AroundTheBoardMode.tsx
@@ -9,57 +9,25 @@ export default function AroundTheBoardMode() {
     const [player2Position, setPlayer2Postiion]= useState(1);
     const [shotCounter, setShotCounter] = useState<number>(0);
     const [player1IsNext, setPlayer1IsNext] = useState<boolean>(true);
-    useEffect(() => {
-        console.log('\n\n\n\@@@@@@@@@@@@@@@'+shotCounter)
-        if (shotCounter > 2) {
-            setPlayer1IsNext(!player1IsNext);
-            setShotCounter(0);
-        }
-    },[shotCounter])
 
     
 
     function onTileClick(index: number) {
         console.log('clicked')
+        let newShotCounter = shotCounter + 1;
+        console.log(newShotCounter)
+        setShotCounter(newShotCounter);
+        const playerPosition = player1IsNext? player1Position : player2Position
+        if (index === playerPosition) {
+            let nextPosition = playerPosition + 1
+            player1IsNext? setPlayer1Postiion(nextPosition) : setPlayer2Postiion(nextPosition)
+        }
 
-        // setShotCounter(prevShotCounter => prevShotCounter + 1)
-        // console.log(shotCounter)
-        // setShotCounter(prevShotCounter => {
-        //     let nextShotCounter = prevShotCounter + 1
-        //     console.log(nextShotCounter)
-
-        //     if (nextShotCounter >= 2) {
-        //         setPlayer1IsNext(!player1IsNext);
-        //         return 0;
-        //     }
-        //     console.log(nextShotCounter)
-        //     return nextShotCounter;
-        // })
-
-        if (shotCounter >= 2) {
+        if (newShotCounter >= 3) {
             // console.log('shotCounter >= 2 + shotCounter: ' + shotCounter)
             setShotCounter(0);
             setPlayer1IsNext(!player1IsNext);
         }
-        else {
-            // console.log('shotCounter < 2 + shotCounter: ' + shotCounter)
-            setShotCounter(shotCounter + 1);
-
-        }
-        // const newTiles = currentPlayer === 1 ? [...player1TileArray] : [...player2TileArray];
-        // console.log('onCLICKLKK')
-        // if (index === player1Position && player1IsNext) {
-        //     console.log('player1Position')
-        // }
-        // if (index === player2Position && !player1IsNext) {
-        //     console.log('player2Position')
-        // }
-
-        // seperatingIncrement()
-        
-        
-
-        
 
     }
     function onClick() {
@@ -74,20 +42,13 @@ export default function AroundTheBoardMode() {
     function onX2Click() {
       //quickdebugconsole.log("X2 clicked")
       }
-// const tilesArray = currentPlayer === 1 ? player1TileArray : player2TileArray;
   return (
     <div className='standard-board'>
         <div className='standard-board-first-row'><ScoreBoard name="Caolan" isAroundTheBoardMode={true} playerTurn={player1IsNext}/> <ScoreBoard name='Dad' isAroundTheBoardMode={true} playerTurn={!player1IsNext}/></div>
 
         <div className='main'>
             <div className='game-container'>
-                {/* {player1IsNext? player1Tiles: player2Tiles} */}
-                {/* {tilesArray.map((isVisible, index) => (
-                    <NumberTile num={index + 1} isHidden={!isVisible} onTileClick={() => onTileClick(index)} onX3Click={onX3Click} onX2Click={onX2Click}/>
-                ))} */}
                 <AroundTheBoardVisibleTiles current={player1Position} onTileClick={onTileClick} onX2Click={onX2Click} onX3Click={onX3Click}/>
-                {/* {player1TileArray}<br/>
-                {player2TileArray}<br/> */}
 
             </div>
         </div>
--- src/AroundTheBoardVisibleTiles.tsx
+++ src/AroundTheBoardVisibleTiles.tsx
@@ -10,31 +10,54 @@ interface AroundTheBoardVisibleTilesProps {
 
 
 export default function AroundTheBoardVisibleTiles({current = 1, onTileClick, onX2Click, onX3Click}: AroundTheBoardVisibleTilesProps) {
-    const [tileArray, setTileArray] = useState<JSX.Element[]>([]);
-
-    const numArray: number[] = Array.from({ length: 20 }, (_, index) => index + 1);
-    useEffect(() => {
-        let tileArrayInitial = [... tileArray];
-        for (let i = current; i < current + 3; i++) {
-            tileArrayInitial.push(
-                <NumberTile
-                    num={i}
-                    isHidden={false}
-                    onTileClick={onTileClick ? onTileClick : () => {}}
-                    onX3Click={onX3Click ? onX3Click : () => {}}
-                    onX2Click={onX2Click ? onX2Click : () => {}}
-                />
-            );
-        }
-        setTileArray(tileArrayInitial);
+    // const [tileArray, setTileArray] = useState<JSX.Element[]>([]);
+    let isCurrentPlusTwoOverTwenty = (current + 2) > 20
+    let isCurrentPlusOneOverTwenty = (current + 1) > 20
+    let isCurrentOverTwenty = current > 20
+    // useEffect(() => {
+    //     let tileArrayInitial = [... tileArray];
+    //     for (let i = current; i < current + 3; i++) {
+    //         tileArrayInitial.push(
+    //             <NumberTile
+    //                 num={i}
+    //                 isHidden={false}
+    //                 onTileClick={onTileClick ? onTileClick : () => {}}
+    //                 onX3Click={onX3Click ? onX3Click : () => {}}
+    //                 onX2Click={onX2Click ? onX2Click : () => {}}
+    //             />
+    //         );
+    //     }
+    //     setTileArray(tileArrayInitial);
 
     
         
-    }, []);
+    // }, [current, onTileClick]);
     
 
-    return (<>
-        {tileArray}
+    return (
+        <>
+            {/* {tileArray} */}
+            <NumberTile
+                num={current}
+                isHidden={isCurrentOverTwenty}
+                onTileClick={onTileClick ? onTileClick : () => {}}
+                onX3Click={onX3Click ? onX3Click : () => {}}
+                onX2Click={onX2Click ? onX2Click : () => {}}
+            />
+            <NumberTile
+                num={current + 1}
+                isHidden={isCurrentPlusOneOverTwenty}
+                onTileClick={onTileClick ? onTileClick : () => {}}
+                onX3Click={onX3Click ? onX3Click : () => {}}
+                onX2Click={onX2Click ? onX2Click : () => {}}
+            />
+            <NumberTile
+                num={current + 2}
+                isHidden={isCurrentPlusTwoOverTwenty}
+                onTileClick={onTileClick ? onTileClick : () => {}}
+                onX3Click={onX3Click ? onX3Click : () => {}}
+                onX2Click={onX2Click ? onX2Click : () => {}}
+            />
         </>
     );
 }
